0757bd5de867bf4fefde5768d8c75239cfd20c50
Nicolas <nicolas.lima.tech@gmail.com>
2025-08-27 12:35:35 -0300

    feat: adding images for using in components | feat: structuring the skeleton of project (main general purpose pages)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {  Routes, Route, BrowserRouter, Link } from 'react-router-dom'
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Costs Project</h1>
-    </div>
+    <BrowserRouter> 
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Company</Link>
+        <Link to="/Contact">Contact</Link>
+        <Link to="/NewProject">New Project</Link>
+      </ul>
+        <Container>
+          <Routes>
+            <Route path= "/" element={<Home/>}/> 
+            <Route path= "/Company" element={<Company/>}/> 
+            <Route path= "/Contact" element={<Contact/>}/> 
+            <Route path= "/NewProject" element={<NewProject/>}/>    
+          </Routes>
+        </Container>
+      <p>Footer</p>
+
+    </BrowserRouter>
   );
 }
 
-export default App;
+export default App
