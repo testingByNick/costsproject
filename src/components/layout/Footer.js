f5e733fa34daa91b2ee6cd3e4b39b985c280a35d
Nicolas <nicolas.lima.tech@gmail.com>
2025-08-28 14:20:32 -0300

    feat: footer and navbar functionalities | feat: buttons functionalities and you directionality

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom"
 import Container from "./Container"
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa"
+import styles from "./Footer.module.css"
 
 
 
 function Footer(){
     return(
-        <div>
-
-        </div>
+        <footer className={styles.footer}>
+            <ul className={styles.socialList}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram />
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+                <li>
+                    <FaGithub />
+                </li>
+            </ul>
+            <p className={styles.copyRight}><span>Costs</span> &copy; 2025</p>
+        </footer>
     )
 }
 
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,21 +7,21 @@ import styles from "./NavBar.module.css"
 
 function NavBar(){
     return(
-        <nav class={styles.navBar}>
+        <nav className={styles.navBar}>
             <Container>
                         <Link to="/"><img src={logo} alt="Costs" /></Link>
-                <ul class={styles.list}>
-                    <li class ={styles.item}>
+                <ul className={styles.list}>
+                    <li className ={styles.item}>
                     <Link to="/">Home</Link>
                     </li>
-                    <li class ={styles.item}>
-                    <Link to="/contact">Contact</Link>
-                    </li>
-                    <li class ={styles.item}>
+                    <li className ={styles.item}>
                     <Link to="/company">Company</Link>
                     </li>
-                    <li class ={styles.item}>
-                    <Link to="/newproject">NewProject</Link>
+                    <li className ={styles.item}>
+                    <Link to="/projects">Projects</Link>
+                    </li>
+                    <li className ={styles.item}>
+                    <Link to="/contact">Contact</Link>
                     </li>
                 </ul>
             </Container>
