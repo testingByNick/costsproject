d8b3d98ee6474c2be4a51474591a9c0b13a48ffe
Nicolas <nicolas.lima.tech@gmail.com>
2025-08-29 15:21:47 -0300

    feat: initial projectcard

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,17 +2,33 @@ import { useLocation } from "react-router-dom"
 import Container from "../layout/Container"
 import LinkButton from "../layout/LinkButton"
 import Message from "../layout/Message"
+import Project from "../project/ProjectCard"
+import { useState, useEffect } from "react"
+import ProjectCard from "./ProjectCard"
 import styles from "../pages/Projects.module.css"
 
 
-
 function Projects() {
+
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch("http://localhost:5000/projects", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {})
+        .catch((err) => console.log(err))
+    }, [])
 
     return (
         <div className={styles.projectContainer}>
