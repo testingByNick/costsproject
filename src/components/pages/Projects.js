0dda132bd0d31d849dff47f5a4b9fe1e377f353c
Nicolas <nicolas.lima.tech@gmail.com>
2025-08-29 15:07:03 -0300

    refactor: changes on style of linkbutton and my projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,20 +1,29 @@
 import { useLocation } from "react-router-dom"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 import Message from "../layout/Message"
+import styles from "../pages/Projects.module.css"
 
 
 
-function Projects(){
+function Projects() {
 
     const location = useLocation()
     let message = ''
-    if(location.state){
+    if (location.state) {
         message = location.state.message
     }
 
-    return(
-        <div>
-        <h1>My Projects</h1>
-        {message && <Message type="success" msg={message}/>}
+    return (
+        <div className={styles.projectContainer}>
+            <div className={styles.titleContainer}>
+                <h1>My Projects</h1>
+                <LinkButton to="/newproject" text="Create new Project"></LinkButton>
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projects...</p>
+            </Container>
         </div>
     )
 }
